f5682415643a79a762a4db8c29045528d1448839
xinyangan <87919526+xinyangan@users.noreply.github.com>
2021-11-16 18:54:04 +0800

    Update Newman.js

--- NewMan.js
+++ NewMan.js
@@ -1,1 +1,9 @@
-newman run learning.postman_collection.json -e baidu.postman_environment.json -r htmlextra
+const newman = require('newman');
+newman.run({
+    collection: require('postman_collection.json'),
+    environment: require('postman_environment.json'),
+    reporters: 'html'
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
